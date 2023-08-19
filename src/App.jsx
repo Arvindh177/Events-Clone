3e756ec47aa53622d4f2a6f1b3fc2a37798b2e12
Arvindh <arvindhdevlps17@gmail.com>
2023-08-20 00:31:00 +0530

    Moving to api

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,20 @@ import './App.css'
 import {Routes,Route} from "react-router-dom"
 import IndexPage from './pages/IndexPage'
 import LoginPage from './pages/LoginPage'
+import Layout from './Layout'
+import RegisterPage from './pages/RegisterPage'
 
 function App() {
   
 
   return (
       <Routes>
-        <Route index element={<IndexPage/>}/>
+        <Route path='/' element={<Layout/>}>
+        
         <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/register' element={<RegisterPage/>}/>
+        </Route>
+        
       </Routes>
 
     
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 export default function Header() {
     return (
         <header className='p-4 flex justify-between'>
@@ -21,7 +22,7 @@ export default function Header() {
         </button>
 
         </div>
-        <Link to={'/Login'}className='flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4 '>
+        <Link to={'/login'}className='flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4 '>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
           </svg>
