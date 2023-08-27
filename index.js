ec259ee906ee89272050355c0834b09ac5db92d8
Arvindh <arvindhdevlps17@gmail.com>
2023-08-27 17:13:13 +0530

    MongoDB setup

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 const express = require('express')
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/User'); 
+require('dotenv').config()
+
 
 app.use(express.json());
 
-app.use(cors({
-    credentials: true,
-    origin: 'http://127.0.0.1:5173/',
-}));
+const corsOption = {
+  origin: 'http://127.0.0.1:5173/',
+  optionsSuccessStatus: 200,
+}
+
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URL); 
 
 app.get('/test', (req,res) => {
     res.json('test ok');
-});
+});  
 
 app.post('/register', (req,res) => {
    const {name , email , password} = req.body;
 
+   User.create({
+    name,
+    email,
+    password,
+   })
+
     res.json({name,email,password});
 })
-
-app.listen(4000);
+ 
+app.listen(4000); 
