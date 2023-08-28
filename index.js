4e4eaf1f7b4510c3d07aa265d161d454b33a1410
Arvindh <arvindhdevlps17@gmail.com>
2023-08-28 19:59:09 +0530

    Partial Login

--- index.js
+++ index.js
@@ -3,17 +3,20 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User'); 
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
+
+const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = 'fasefraw4r5r3wq45wdfgw34twdfg';
 
 
 app.use(express.json());
 
-const corsOption = {
-  origin: 'http://127.0.0.1:5173/',
-  optionsSuccessStatus: 200,
-}
-
-app.use(cors());
+app.use(cors({
+  credentials:true,
+  origin: 'http://127.0.0.1:5173',
+}));
 
 mongoose.connect(process.env.MONGO_URL); 
 
@@ -21,16 +24,43 @@ app.get('/test', (req,res) => {
     res.json('test ok');
 });  
 
-app.post('/register', (req,res) => {
-   const {name , email , password} = req.body;
+app.post('/register', async (req,res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const {name,email,password} = req.body;
 
-   User.create({
-    name,
-    email,
-    password,
-   })
+  try {
+    const userDoc = await User.create({
+      name,
+      email,
+      password:bcrypt.hashSync(password, bcryptSalt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    res.status(422).json(e);
+  }
 
-    res.json({name,email,password});
-})
+});
+
+app.post('/login', async (req,res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const {email,password} = req.body;
+  const userDoc = await User.findOne({email});
+  if (userDoc) {
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      jwt.sign({
+        email:userDoc.email,
+        id:userDoc._id
+      }, jwtSecret, {}, (err,token) => {
+        if (err) throw err;
+        res.cookie('token', token).json(userDoc);
+      });
+    } else {
+      res.status(422).json('pass not ok');
+    }
+  } else {
+    res.json('not found');
+  }
+});
  
 app.listen(4000); 
