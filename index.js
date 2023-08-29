ae9d2af1009c603b6622bcf9eeebbd6ca0e996a1
Arvindh <arvindhdevlps17@gmail.com>
2023-08-30 04:25:32 +0530

    Cookie error

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const User = require('./models/User'); 
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
 require('dotenv').config()
 
 const bcryptSalt = bcrypt.genSaltSync(10);
@@ -12,6 +13,7 @@ const jwtSecret = 'fasefraw4r5r3wq45wdfgw34twdfg';
 
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use(cors({
   credentials:true,
@@ -50,10 +52,14 @@ app.post('/login', async (req,res) => {
     if (passOk) {
       jwt.sign({
         email:userDoc.email,
-        id:userDoc._id
+        id:userDoc._id,
       }, jwtSecret, {}, (err,token) => {
         if (err) throw err;
-        res.cookie('token', token).json(userDoc);
+        res.cookie('token', token,
+        {
+          httpOnly: true,
+          sameSite: 'strict',
+        }).json(userDoc);
       });
     } else {
       res.status(422).json('pass not ok');
@@ -63,11 +69,21 @@ app.post('/login', async (req,res) => {
   }
 });
 
-app.get('/profile',(req ,res) => {
-
+app.get('/profile', (req,res) => {
+  mongoose.connect(process.env.MONGO_URL);
   const {token} = req.cookies;
-  res.json({token});
-  res.json('user info')
-})
+  if (token) {
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+      if (err) {
+        throw err; }
+        else {
+      const {name,email,_id} = await User.findById(userData.id);
+      res.json({name,email,_id});
+        }
+    });
+  } else {
+    res.json(null);
+  }
+});
  
 app.listen(4000); 
