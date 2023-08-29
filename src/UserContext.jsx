7a133cdb4236594fff542fb9481a81f613b51851
Arvindh <arvindhdevlps17@gmail.com>
2023-08-30 04:26:06 +0530

    Cookie error

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { createContext, useEffect, useState } from 'react';
+import {data} from "autoprefixer";
 
 export const UserContext = createContext({});
 
@@ -7,7 +8,10 @@ export function UserContextProvider({children}) {
     const [user,setUser] = useState(null); 
   useEffect(() => {
       if(!user) {
-          axios.get('/profile')
+         axios.get('/profile').then(({data}) => {
+            setUser(data);
+         });
+          
      }
   }, []); 
   
