54ed424d43a2cff1d67853aef3cdb043764199f2
Arvindh <arvindhdevlps17@gmail.com>
2023-09-19 19:47:44 +0530

    Upload by image 90%

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const User = require('./models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser')
+const imageDownloader = require('image-downloader');
 require('dotenv').config()
 
 const bcryptSalt = bcrypt.genSaltSync(10);
@@ -14,6 +15,7 @@ const jwtSecret = 'fasefraw4r5r3wq45wdfgw34twdfg';
 
 app.use(express.json());
 app.use(cookieParser());
+app.use('/uploads', express.static(__dirname+'/uploads'));
 
 app.use(cors({
   credentials:true,
@@ -85,5 +87,17 @@ app.get('/profile', (req,res) => {
 app.post('/logout', (req,res) => {
   res.cookie('token', '').json(true);
 })
+
+app.post('/upload-by-link', async (req,res) => {
+  const {link} = req.body;
+  const newName = 'photo' + Date.now() + '.jpeg'
+ await imageDownloader.image({
+  url: link,
+  dest: __dirname+ '/uploads' + newName, 
+ });
+
+ res.json(newName);
+
+} )
  
 app.listen(4000); 
