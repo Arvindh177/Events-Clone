21c0ecb91bcb3d02cec70e012a0c82f0e16e46d3
Arvindh <arvindhdevlps17@gmail.com>
2023-10-02 02:08:34 +0530

    Partial commit

--- src/pages/PlacesPage.jsx
+++ src/pages/PlacesPage.jsx
@@ -44,6 +44,23 @@ async function addPhotoByLink(ev) {
      setPhotoLink('');
 }
 
+
+function uploadPhoto(ev){
+    const files = ev.target.files;
+    const data = new FormData();
+    for(let i=0;i<files.length;i++){
+        data.append('photos',files[i]);
+    }
+    
+    axios.post('/upload', data, {
+        headers: {'Content-type':'multipart/form-data'} 
+    }).then(response => {
+        const {data:filenames} = response;
+        setAddedPhotos(prev => {
+            return [...prev, ...filenames];
+         })
+    })
+}
     
     return(
         <div>
@@ -72,17 +89,19 @@ async function addPhotoByLink(ev) {
                         <input type="text" value={photoLink} onChange={ev => setPhotoLink(ev.target.value)} placeholder='Add using a link ... .jpeg'/> 
                         <button onClick={addPhotoByLink} className="bg-pray-200 grow px-4 rounded-2xl">Add photo</button>
                     </div>
+                    
                     <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
                         {addedPhotos.length > 0 && addedPhotos.map(link => (
                             <div>
                                 <img className="rounded-2xl" src={'http://localhost:4000/uploads/'+link}/>
                                 </div>
                         ))}
-                    <button className="border items-center bg-transparent justify-center gap-1 flex rounded-2xl p-2 text-2xl text-gray-600 mb-3">
+                    <label className="cursor-pointer border items-center bg-transparent justify-center gap-1 flex rounded-2xl p-2 text-2xl text-gray-600 mb-3">
+                    <input type="file" className="hidden" onChange={uploadPhoto}/>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
                          <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
                     </svg>
-                    Upload</button>
+                    Upload</label>
                     </div>
 
                     {preInput('Description','Add description of the event')}
